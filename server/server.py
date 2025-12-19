c4e5d454c3f86b58c06dc2f6e797a849d21b8e06
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2025-12-19 13:38:43 -0800

    future caddy hardening, I guess

--- server/server.py
+++ server/server.py
@@ -6,11 +6,13 @@ import os
 import queue
 import time
 from flask_cors import CORS
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 dotenv.load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1)
 latest = None
 lock = threading.Lock()
 
