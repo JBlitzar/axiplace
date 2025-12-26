832630b916582737c7a4b6b3523f02e53a841693
jb-pi <jb-pi@raspberrypi>
2025-12-26 15:14:08 -0800

    fix scary noises

--- rpi/draw.py
+++ rpi/draw.py
@@ -14,10 +14,18 @@ if not connected:
 
 ad.penup()
 
-cell2inch = 11 / 32 # 11 inches per 32 cells
+cell2inch = 11 / 22 # 11 inches per 22 cells
 banned_cells = {(0,0), (31,0), (0,17), (31,17)}
 def bezier(cell_x, cell_y, params):
+    connected = ad.connect()
+
+    if not connected:
+        ad.disconnect()
+        print("Could not connect to plotter!")
+        exit(1)
+    
     if (cell_x, cell_y) in banned_cells:
+        ad.disconnect()
         return
     
 
@@ -28,17 +36,17 @@ def bezier(cell_x, cell_y, params):
     x4, y4 = params[3]
     flattened = [x1, y1, x2, y2, x3, y3, x4, y4]
     if any(v is None for v in flattened):
+        ad.disconnect()
         return
     if any(not (0 <= v <= 1) for v in flattened):
+        ad.disconnect()
         return
     ad.penup()
     points = 100
     flag = False
     
     for t in np.linspace(0, 1, points):
-        if not flag:
-            flag = True
-            ad.pendown()
+        
         x = (
             (1 - t) ** 3 * x1
             + 3 * (1 - t) ** 2 * t * x2
@@ -52,19 +60,24 @@ def bezier(cell_x, cell_y, params):
             + t ** 3 * y4
         )
         ad.goto((cell_x + x) * cell2inch, (cell_y + y) * cell2inch)
+        if not flag:
+            flag = True
+            ad.pendown()
 
-    ad.penup()
-    ad.goto(0, 10)
-
-
-def cleanup(signum=None, frame=None):
-    print("\nCleaning up...")
     ad.penup()
     ad.goto(0, 0)
     ad.disconnect()
-    print("Done!")
-    os.system("axi off")
-    sys.exit(0)
 
-signal.signal(signal.SIGTERM, cleanup)
-signal.signal(signal.SIGINT, cleanup)
+
+
+# def cleanup(signum=None, frame=None):
+#     print("\nCleaning up...")
+#     ad.penup()
+#     ad.goto(0, 0)
+#     ad.disconnect()
+#     print("Done!")
+#     os.system("axi off")
+#     sys.exit(0)
+
+# signal.signal(signal.SIGTERM, cleanup)
+# signal.signal(signal.SIGINT, cleanup)
